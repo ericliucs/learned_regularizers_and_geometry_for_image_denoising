87e3a75d650310f997521fefe5bc0758b3921e77
Ryan Cecil <62820013+cecil-ryan@users.noreply.github.com>
2022-08-18 15:11:30 -0400

    Delete models_to_server.sh

--- models_to_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#echo Attempting to back up files
-#echo Make sure to change old version!!!
-#ssh collaborators@mathcs.duq.edu 'bash'< backup_script.sh
-rsync -r -av --progress denoising/models/saved_models collaborators@mathcs.duq.edu:/work/collaborators/Ryan/learned_regularizers_and_geometry_for_image_denoising/denoising/models
-echo Code has been updated
